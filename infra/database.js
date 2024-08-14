3781195de735484c0470ae56011a69968d72a9b9
Bernardo Barro <63940272+BernardoBarro@users.noreply.github.com>
2024-08-14 00:25:25 +0000

    dia 18 - add `database.js` and `.env` pois é

---
+++ infra/database.js
@@ -0,0 +1,19 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  }); //sincrono cria uma instancia de um Client
+  await client.connect(); //assincrono, estamos requisitando uma conexão ao banco de dados
+  const result = await client.query(queryObject); //assincrono realiza a query enviada para a função
+  await client.end(); //finaliza a sessão do banco de dados
+  return result;
+}
+
+export default {
+  query: query,
+};
