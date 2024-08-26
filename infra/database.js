ae09518a0d8d0e5daf1b29a342e6515074e7eb9c
Bernardo Barro <63940272+BernardoBarro@users.noreply.github.com>
2024-08-26 22:41:22 +0000

    Dia 21 makes `database.js` `clients.connect()` more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -8,13 +8,21 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   }); //sincrono cria uma instancia de um Client
+  console.log("Dados do banco: ", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
-  await client.connect(); //assincrono, estamos requisitando uma conexão ao banco de dados
   try {
+    await client.connect(); //assincrono, estamos requisitando uma conexão ao banco de dados
     const result = await client.query(queryObject); //assincrono realiza a query enviada para a função
     return result;
   } catch (error) {
     console.log(error);
+    throw error;
   } finally {
     await client.end(); //finaliza a sessão do banco de dados
   }
