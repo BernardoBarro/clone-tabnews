919bd347dc92af2b877119f3ce5141d37018a520
Bernardo Barro <63940272+BernardoBarro@users.noreply.github.com>
2024-08-27 00:54:57 +0000

    Dia 21 remove database data log

--- infra/database.js
+++ infra/database.js
@@ -9,13 +9,6 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     ssl: process.env.NODE_ENV === "development" ? false : true,
   }); //sincrono cria uma instancia de um Client
-  console.log("Dados do banco: ", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
 
   try {
     await client.connect(); //assincrono, estamos requisitando uma conexão ao banco de dados
